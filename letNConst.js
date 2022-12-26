5ed931af9b2bd092203ccec0bb6beb6a07764824
alex <alexisoro99@gmail.com>
2022-12-26 12:49:54 -0800

    adding files

---
+++ letNConst.js
@@ -0,0 +1,12 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+const PI = 3.14
+
+//1. What is the difference between var and let?
+    // Let  can not be redeclared but it can still change, where as var can be changed while also being redeclared. 
+//2. What is the difference between var and const?
+    // Const can never be redeclared or changed only the contents within it can be changed such as a variable is claimed to be an Array. 
+//3. What is the difference between let and const?
+    // Once a varaible is declared let will alow you to change it, where as const makes that variable constant it can no longer be changed.
+//4. What is hoisting?
+    // It is the order in which a variable is initialized dependiong if var, let or const is used.
